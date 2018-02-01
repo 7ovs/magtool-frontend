2770a34defc7b9a7a21b50b918c91f59c1efc271
rpm42 <rpm42@yandex.com>
2018-02-01 14:04:38 +0300

    update Control page, add 3 buttons

--- src/components/Control.vue
+++ src/components/Control.vue
@@ -1,13 +1,65 @@
 <template>
-  <v-container fluid fill-height>
-    <v-layout justify-center align-center>
-      <span class="white--text">Control</span>
+  <v-container fluid class="text-xs-left">
+    <v-layout row class="mb-4" align-content-start justify-start>
+      <v-flex md4><v-subheader>Проверка доступности сервера</v-subheader></v-flex>
+      <v-flex md1><v-progress-circular v-if="pingPending" indeterminate :width="7" color="red darken-4"></v-progress-circular></v-flex>
+      <v-flex md2><v-btn color="red darken-4" block @click="ping">PING</v-btn></v-flex>
+    </v-layout>
+    <v-layout row class="mb-4">
+      <v-flex md4><v-subheader>Отчиска кэша magento и перезапуск кэш-сервера varnish</v-subheader></v-flex>
+      <v-flex md1><v-progress-circular v-if="cleanCachePending" indeterminate :width="7" color="red darken-4"></v-progress-circular></v-flex>
+      <v-flex md2><v-btn color="red darken-4" block @click="cleanCache">CLEAN CACHE</v-btn></v-flex>
+    </v-layout>
+    <v-layout row class="mb-4">
+      <v-flex md4><v-subheader>Получить лог сервера magtool</v-subheader></v-flex>
+      <v-flex md1><v-progress-circular v-if="getLogPending" indeterminate :width="7" color="red darken-4"></v-progress-circular></v-flex>
+      <v-flex md2><v-btn color="red darken-4" block @click="getLog">GET LOG</v-btn></v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
+import config from '@/config.json'
 export default {
-  name: 'Control'
+  name: 'Control',
+  data () {
+    return {
+      pingPending: false,
+      cleanCachePending: false,
+      getLogPending: false
+    }
+  },
+  methods: {
+    ping () {
+      this.pingPending = true
+      this.$http.post(`${config.backend_base}/control`, { command: 'PING' }).then(({ data }) => {
+        this.pingPending = false
+        console.log('PING COMPETE', data)
+      }).catch(err => {
+        this.pingPending = false
+        console.log('PING FAIL', err)
+      })
+    },
+    getLog () {
+      this.getLogPending = true
+      this.$http.post(`${config.backend_base}/control`, { command: 'GET_LOG' }).then(({ data }) => {
+        this.getLogPending = false
+        console.log('GET_LOG COMPETE', data)
+      }).catch(err => {
+        this.getLogPending = false
+        console.log('GET_LOG FAIL', err)
+      })
+    },
+    cleanCache () {
+      this.cleanCachePending = true
+      this.$http.post(`${config.backend_base}/control`, { command: 'CLEAN_CACHE' }).then(({ data }) => {
+        this.cleanCachePending = false
+        console.log('CLEAN_CACHE COMPETE', data)
+      }).catch(err => {
+        this.cleanCachePending = false
+        console.log('CLEAN_CACHE FAIL', err)
+      })
+    }
+  }
 }
 </script>
